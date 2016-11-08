3c031f6ce5617bde0f68ed5c31c4ee69f8ca9c94
Madeline Huberth <madelinehuberth@gmail.com>
2016-11-08 13:55:03 -0800

    made block creation updates: fixed trigger enumeration, removed commas except first in coll.txt files, changed up/down deviant enumeration from 1/2 to -2/2

--- stimuli/block_making/make_experiment_blocks.sh
+++ stimuli/block_making/make_experiment_blocks.sh
@@ -1,33 +1,35 @@
-#!/bin/sh
 #Madeline Huberth, 10/16/16
-#this makes the files block{1..12}.coll.txt for Max to read in
+#Piano Duet study: this script makes the files block{1..12}.coll.txt for Max to read in
+
 #to run:
-# >> ./make_experiment_blocks.sh blockorder_VERSION.csv deviants.csv pairfolder
-# for example: ./make_experiment_blocks.sh blockorder_11.csv deviants.csv MW_TD
-#gshuf is the mac version of the command shuf
-# use brew install coreutils to get gshuf on a mac
+# >> ./make_experiment_blocks.sh blockorder_VERSION.csv pairfolder
+# for example: ./make_experiment_blocks.sh blockorder_11.csv MW_TD
 
 #The coll file orders the information in the following left to right columns:
-#trial number
-#score, 
-#which keyboard first (odd/even), 
-#which phrase of keyboard 1 is the deviant in (1/2), 
-#which phrase of keyboard 2 is the deviant in (1/2), 
-#which note for keyboard 1 is the deviant in (4/5), 
-#which note for keyboard 2 is the deviant in (4/5), 
-#does the deviant descend or ascend for keyboard 1 (1/2), 
-#does the deviant descend or ascend for keyboard 2 (1/2)
+#trial number,
+#score
+#which keyboard first (odd/even)
+#which phrase of keyboard 1 is the deviant in (1/2) 
+#which phrase of keyboard 2 is the deviant in (1/2)
+#which note for keyboard 1 is the deviant in (4/5) 
+#which note for keyboard 2 is the deviant in (4/5) 
+#does the deviant descend or ascend for keyboard 1 (-2/2) 
+#does the deviant descend or ascend for keyboard 2 (-2/2)
 #trigger code identifying the trial
 
+##REQUIREMENTS TO RUN
+#bash 4.2 and above
+
+
 BLOCKORDER=$1
-DEVIANTS=$2
 [ ! -f $BLOCKORDER ] && { echo "$BLOCKORDER file not found"; exit 99; }
-[ ! -f $DEVIANTS ] && { echo "$DEVIANTS file not found"; exit 99; }
+[ ! -f "deviants.csv" ] && { echo "deviants file not found"; exit 99; }
+[ ! -f "trigger_logic.txt" ] && { echo "trigger logic file not found"; exit 99; }
 
 #make block files
 for((i = 1; i <= 12; i++)); do
 	#name the block
-	filename="$3/block$i.coll.txt"
+	filename="$2/block$i.coll.txt"
 	#if the block trial files are already made, quit
 	[ -f $filename ] && { echo "$filename already made"; exit 99; }
 	#make block file
@@ -41,19 +43,29 @@ for((i = 1; i <= 12; i++)); do
 	do
 		updown1=$(( ( RANDOM % 2 )  + 1 ))
 		updown2=$(( ( RANDOM % 2 )  + 1 ))
-	    echo "$block, $line, $updown1, $updown2," >> $filename
-	done < $DEVIANTS
+		if [ $updown1 == 1 ]; then
+			ud1=-2;
+		elif [ $updown1 == 2 ]; then
+			ud1=2;
+		fi
+		if [ $updown2 == 1 ]; then
+			ud2=-2;
+		elif [ $updown2 == 2 ]; then
+			ud2=2;
+		fi
+	    echo "$block, $line, $ud1, $ud2," >> $filename
+	done < "deviants.csv"
 
 	#shuffle
 	#gshuf -o $filename < $filename
-	tmp="$3/tmp$i.txt"
+	tmp="$2/tmp$i.txt"
 	touch "$tmp"
 	cat $filename | while read i;
 	do echo "$RANDOM $i";
 	done | sort | sed "s/^[0-9]+ //" > $tmp; mv $tmp $filename
 	
 	#add line order in beginning
-	filename_tmp="$3/block_tmp_$i.coll.txt"
+	filename_tmp="$2/block_tmp_$i.coll.txt"
 	awk '{printf "%d, %s\n", NR, $0}' $filename > $filename_tmp && mv $filename_tmp $filename
 	
 done
@@ -63,7 +75,7 @@ done
 #reads in and parses each trial for each block, and checks against every line of the trial_logic.txt file
 #when a match is found, a new file is made with all relevent info
 for((i = 1; i <= 12; i++)); do
-	filename="$3/block$i.coll.txt"
+	filename="$2/block$i.coll.txt"
 	limenum=1;
 	while read line
 	do
@@ -83,9 +95,9 @@ for((i = 1; i <= 12; i++)); do
 		fi
 		#human/max
 		if (( (i >= 5 ) && (i <= 8 ) )) ; then
-			hummax=2;
-		else
 			hummax=1;
+		else
+			hummax=2;
 		fi
 		#keyboard 1 first or second
 		if [ "${arr[3]}" == "1," ]; then
@@ -106,7 +118,7 @@ for((i = 1; i <= 12; i++)); do
 			K2dev=2;
 		fi		
 		
-		dev_filename="$3/block$i.dev.txt"
+		dev_filename="$2/block$i.dev.txt"
 	   	touch "$dev_filename"
 		
 		while read trigline
@@ -115,7 +127,9 @@ for((i = 1; i <= 12; i++)); do
 			if (( ( $hummax == ${trig[0]} ) && ( $samediff == ${trig[1]} ) && ( $K1 == ${trig[2]} ) && ( $K1dev == ${trig[3]} ) && ( $K2dev == ${trig[4]} ) )); then
 				#paste into new file - note that the sorting process from above results in a random number appearing in the intermediate file - this is
 				#skipped over here by passing {arr[1]}
-				( echo ${arr[0]}" "${arr[2]}" "${arr[3]}" "${arr[4]}" "${arr[5]}" "${arr[6]} "" ${arr[7]} "" ${arr[8]} "" ${arr[9]} "" ${trig[5]}";" ) >> $dev_filename
+				
+				##  the syntax ::-1 removes the commas from the strings
+				( echo ${arr[0]}" "${arr[2]::-1}" "${arr[3]::-1}" "${arr[4]::-1}" "${arr[5]::-1}" "${arr[6]::-1} "" ${arr[7]::-1} "" ${arr[8]::-1} "" ${arr[9]::-1} "" ${trig[5]}";" ) >> $dev_filename
 			fi
 		done < "trigger_logic.txt"
 	done < $filename
@@ -123,6 +137,3 @@ for((i = 1; i <= 12; i++)); do
 	mv $dev_filename $filename
 
 done
-
-
-
